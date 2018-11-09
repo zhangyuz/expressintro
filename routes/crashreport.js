e84d230bf92c9f88a96bb4dd1baf72cac0376e39
Zhang Yuzheng <me@zhanyuzheng.com>
2018-11-09 19:17:01 +0800

    save uploaded file to mongodb
    Signed-off-by: Zhang Yuzheng <me@zhanyuzheng.com>

--- routes/crashreport.js
+++ routes/crashreport.js
@@ -3,8 +3,7 @@ var CrashReport = require('../models/CrashReport');
 var multer = require('multer');
 const FIELDNAMEATTACHMENTS = 'attachments';
 const FIELDNAMELOGFILE = 'logfile';
-var memStorage = multer.memoryStorage();
-var upload = multer({storage: memStorage});
+var upload = multer();
 var crashFields = upload.fields([{ name: FIELDNAMEATTACHMENTS, maxCount: 10 },
     { name: FIELDNAMELOGFILE, maxCount: 1 }]);
 var router = express.Router();
@@ -19,22 +18,23 @@ router.route('/')
         console.log(req.body);
         console.log(req.body.metadata);
         console.log(JSON.parse(req.body.metadata).tag);
+        console.log(JSON.parse(req.body.metadata).summary);
         var crashReport = new CrashReport(JSON.parse(req.body.metadata));
         var fileNameList = '';
         attachments = req.files[FIELDNAMEATTACHMENTS];
         for (i = 0; i < attachments.length; i++) {
             console.log(`${attachments[i].originalname}`);
             fileNameList = fileNameList + attachments[i].originalname + '\n';
+            crashReport.attachments[i] = attachments[i].buffer;
         }
 
         logfiles = req.files[FIELDNAMELOGFILE];
         for (i = 0; i < logfiles.length; i++) {
             console.log(`${logfiles[i].originalname}`);
             fileNameList = fileNameList + logfiles[i].originalname + '\n';
+            crashReport.logFiles[i] = logfiles[i].buffer;
         }
 
-        crashReport.attachments = attachments;
-        crashReport.logFiles = logfiles;
         crashReport.save().then(() => {
             res.send(`Crash Reports\n${req.body.metadata}\n${fileNameList}`);
         });
